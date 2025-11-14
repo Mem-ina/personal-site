0065e8394d65ffd35e1dce992f011bd20710650d
Memina <sangaramemina@gmail.com>
2025-11-14 08:59:25 +0200

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// smooth scrolling when clicking on navigation links
+document.querySelectorAll('header nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
